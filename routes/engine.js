4c702ef8634c0720f3fd50aa550ad577ce95bfe6
shahreaz <ashahreaz@gmail.com>
2021-10-02 00:45:42 +0600

    File upload with multer

--- routes/engine.js
+++ routes/engine.js
@@ -1,4 +1,8 @@
 const router = require("express").Router();
+const fs = require("fs-extra");
+
+// configs
+const upload = require("../configs/multer");
 
 // routes 	GET /api/
 // desc 	The home route
@@ -14,8 +18,18 @@ router.get("/", (req, res) => {
 // routes 	POST /api/parse
 // desc 	the json data parser
 // access 	public
-router.post("/parse", (req, res) => {
-	res.send({ message: "The json data parser" });
+router.post("/parse", upload.single("jsonFile"), async (req, res) => {
+	try {
+		const data = await fs.readJson(req.file.path);
+
+		console.log(data);
+
+		await fs.remove(req.file.path);
+
+		res.send({ message: "successful" });
+	} catch (error) {
+		console.error(error);
+	}
 });
 
 module.exports = router;
